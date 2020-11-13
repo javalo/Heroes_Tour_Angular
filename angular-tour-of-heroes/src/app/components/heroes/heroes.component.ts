35b49570dc34e19d867aa9ad4ac6e1031e41950a
javalo <lo.cheikhna@gmail.com>
2020-11-13 05:32:28 +0100

    Display List Part

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/interfaces/heroe';
+import { HEROES } from 'src/app/mocks/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,18 @@ import { Hero } from 'src/app/interfaces/heroe';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero | undefined;
+
   
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
